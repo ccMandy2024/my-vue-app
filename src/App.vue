abd26d0fea312e55934d41e2d8cae8a98ff2b5ce
Mandy <ccmandy.2024@gmail.com>
2024-11-05 19:55:54 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
 </script>
 
 <template>
   <div>
-    <Button>Click me</Button>
+    <Button>Click me123</Button>
   </div>
 </template>
